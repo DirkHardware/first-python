c634a0d887e1d74ee2e009b39cb068acacc9c8af
DirkHardware <email.the.anderson@gmail.com>
2020-06-02 19:59:14 -0400

    negative slicing

--- .idea/strings.py
+++ .idea/strings.py
@@ -18,3 +18,28 @@ print(parrot[0:6:3]) #Nw
 
 number = "9,223,372,036,854,775,807"
 print(number[1::4]) #,,,,,,
+
+#negative slicing
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+backwards = letters[25::-1]
+print(backwards)
+
+backwards_slice = letters[25:16:-1]
+# or
+backwards_slice2 = letters[:-9:-1]
+print(backwards_slice)
+
+
+#slicing in steps, seperator example
+
+number = "9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
+
+
+# % interpolation
+# print("%s %s %s" %('Hello','World', 'I am Satan.'))
