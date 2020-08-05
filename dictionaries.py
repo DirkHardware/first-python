142a46786faa4da0402f7154e1e4110d629f7657
DirkHardware <email.the.anderson@gmail.com>
2020-08-05 15:49:05 -0400

    skipped ahead to dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+# So it looks like dictionaries work the same way as hashs or objects,
+# Except the keys cannot be string literals if they are not in quotes
+
+factions = {"Space Marines": "Assholes with giant shoulder pads",
+            "Orks": "Cockney Space Hooligans",
+            "Tyranids": "The bugs from Alien",
+            "Imperial Guards": "The true heroes of the Imperium",
+            7: 8}
