c3336d96efad08086e6b5324afbfd904046aa9a2
DirkHardware <email.the.anderson@gmail.com>
2020-08-05 18:36:49 -0400

    iteration and improved key selection

--- dictionaries.py
+++ dictionaries.py
@@ -11,3 +11,39 @@ factions = {"Space Marines": "Assholes with giant shoulder pads",
 
 faction_details = factions["Imperial Guard"]
 print(faction_details)
+
+# Updating a value
+factions["Space Marines"] = "Spehss Muhrines"
+
+print(factions)
+
+# Getting rid of a key/value pair
+del factions[7]
+
+print(factions)
+
+# To clear out the whole dictionary
+# factions.clear()
+# print(factions)
+
+# Watch out when calling values by their keys. A non extant key will
+# throw an error. A better way to call them is by the .get() method
+# which will not throw an error
+# print(factions["Grey Knights"])
+
+# print(factions.get("Grey Knights"))
+
+# .get() can also take a default value to return if the key isn't in the dictionary
+# Example below
+
+# while True:
+#     dict_key = input("Please enter a faction: ")
+#     if dict_key == "quit":
+#         break
+#     description = factions.get(dict_key, "There are no {0} in the 41st millenium".format(dict_key))
+#     print(description)
+
+# Dictionaries are obviously iteratable
+# However it is not guaranteed that they will show up in the same order
+for faction in factions:
+    print(factions[faction])
