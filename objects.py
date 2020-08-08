328c1e7e5c2aa42454c01575feb0658836eb5379
DirkHardware <email.the.anderson@gmail.com>
2020-08-08 17:48:18 -0400

    methods! *explosion noise*

--- objects.py
+++ objects.py
@@ -5,6 +5,9 @@ class Kettle(object):
         self.price = price
         self.on = False
 
+    def switch_on(self):
+        self.on = True
+        print("Your {0.make} is on!".format(self))
 
 kenwood = Kettle("Kenwood", 8.99)
 print(kenwood.make)
@@ -14,5 +17,11 @@ kenwood.price = 12.75
 print(kenwood.price)
 
 hamilton = Kettle("Hamilton", 14.55)
+# Object variables are called attributes
+# The creation of a class instance is called instantiation
+# print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
 
-print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
+
+kenwood.switch_on()
+hamilton.switch_on()
