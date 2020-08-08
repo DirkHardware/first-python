bf8eb90b162d16ff6f902c1f61862f970c078742
DirkHardware <email.the.anderson@gmail.com>
2020-08-08 10:50:17 -0400

    more fun with the turtle lib

--- turtledemo.py
+++ turtledemo.py
@@ -1,6 +1,22 @@
-import turtle
+from turtle import *
+from time import *
+# Apparently you're not supposed to use an import splat bc it might
+# hide one of your classes (since you have no idea what you're importing)
 
-turtle.forward(150)
-turtle.right(250)
-turtle.forward(150)
-turtle.done()
+# turtle.forward(150)
+# sleep(1)
+# turtle.right(250)
+# sleep(1)
+# turtle.forward(150)
+# turtle.done()
+
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    print(pos())
+    if abs(pos()) < 1:
+        break
+end_fill()
+sleep(5)
