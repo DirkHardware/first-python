e0b76ff60911bd76c3e16d71ff291ed7188f707a
DirkHardware <email.the.anderson@gmail.com>
2020-08-08 17:03:38 -0400

    starting objects!

--- functions.py
+++ functions.py
@@ -3,12 +3,12 @@
 import os
 
 
-def centre_text(*args, sep = ' '):
+def centre_text(*args, sep=' ', file=None):
     text = ""
     for arg in args:
         text += str(arg) + sep
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text)
+    print(" " * left_margin, text, file=file)
 
 
 # size = os.get_terminal_size()
@@ -16,7 +16,7 @@ def centre_text(*args, sep = ' '):
 # If we wanted to centre contingent on terminal size, we would
 # run it like this. However this does not work in PyCharm's terminal
 
-def centre_text_relative(*args):
+def centre_text_relative(*args, file=None):
     text = ""
     for arg in args:
         text += str(arg) + " "
@@ -25,4 +25,6 @@ def centre_text_relative(*args):
     print(" " * left_margin, text)
 
 
-centre_text("Star Trekking", "across the universe", sep = ',')
+with open('centered', mode='w') as centered_file:
+
+    centre_text("Star Trekking", "across the universe", file=centered_file)
