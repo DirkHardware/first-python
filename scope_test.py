151a835acca625468bc2de527e8f34e987a2a4e6
DirkHardware <email.the.anderson@gmail.com>
2020-08-21 12:12:13 -0400

    just adding some clarification notes on scope

--- scope_test.py
+++ scope_test.py
@@ -1,12 +1,13 @@
 x = 16
-
+# As we can see, Python does not check one level above for variables
+# the way JS does.
 
 def scopetest2():
     print(x)
 
 
 def scopetest():
-    x=12
+    x = 12
     scopetest2()
 
 
