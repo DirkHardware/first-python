d4ff9d06b22e83595d8aa5afe9fc7e21e810e737
DirkHardware <email.the.anderson@gmail.com>
2020-08-25 14:36:08 -0400

    dynamic grid size, procedural entrances, some render problems

--- procedural.py
+++ procedural.py
@@ -1,15 +1,58 @@
 # Pixel Art - http://www.101computing.net/pixel-art-in-python/
 
 import turtle
+import random
 
 myPen = turtle.Turtle()
 # myPen.tracer(0)
-myPen.speed(50)
+myPen.speed(0)
 myPen.color("#000000")
 
+minX = 20
+minY = 20
+
+grid = [[]]
+for columns in range(0, minX):
+    grid[0].append(0)
+
+for rows in range(0, minY):
+    row = []
+    for columns in range(0, minX):
+        row.append(0)
+    grid.append(row)
+
+# entranceDirection = random.randange(0, 4)
+entranceDirection = 1
+# 0 = North, 1 = East, 2 = South, 3 = West
+if entranceDirection == 0:
+    grid[0][minX // 2] = 1
+    grid[0][(minX // 2) - 1] = 1
+    grid[0][(minX // 2) + 1] = 1
+
+elif entranceDirection == 1:
+    grid[minY // 2][-1] = 1
+    grid[(minY // 2) - 1][-1] = 1
+    grid[(minY // 2) + 1][-1] = 1
 
 # This function draws a box by drawing each side of the square and using the fill function
-def box(intDim):
+def emptyGrid(intDim):
+    # myPen.begin_fill()
+    # 0 deg.
+    myPen.forward(intDim)
+    myPen.left(90)
+    # 90 deg.
+    myPen.forward(intDim)
+    myPen.left(90)
+    # 180 deg.
+    myPen.forward(intDim)
+    myPen.left(90)
+    # 270 deg.
+    myPen.forward(intDim)
+    # myPen.end_fill()
+    myPen.setheading(0)
+
+
+def fillGrid(intDim):
     myPen.begin_fill()
     # 0 deg.
     myPen.forward(intDim)
@@ -25,11 +68,11 @@ def box(intDim):
     myPen.end_fill()
     myPen.setheading(0)
 
-
 boxSize = 10
 # Position myPen in top left area of the screen
 myPen.penup()
-myPen.forward(-100)
+# Below var was originally -100 I think.
+myPen.forward(-(len(grid)*3.5))
 myPen.setheading(90)
 myPen.forward(100)
 myPen.setheading(0)
@@ -45,35 +88,46 @@ myPen.setheading(0)
 
 # Here is how your PixelArt is stored (using a "list of lists")
 
-pixels = [[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]]
-pixels.append([0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0])
-pixels.append([0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0])
-pixels.append([0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0])
-pixels.append([0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0])
-pixels.append([1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1])
-pixels.append([1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1])
-pixels.append([1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1])
-pixels.append([1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1])
-pixels.append([1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1])
-pixels.append([1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1])
-pixels.append([1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1])
-pixels.append([0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0])
-pixels.append([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
-pixels.append([0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0])
-pixels.append([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0])
+# pixels = [[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]]
+# pixels.append([0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0])
+# pixels.append([0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0])
+# pixels.append([0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0])
+# pixels.append([0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0])
+# pixels.append([1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1])
+# pixels.append([1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1])
+# pixels.append([1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1])
+# pixels.append([1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1])
+# pixels.append([1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1])
+# pixels.append([1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1])
+# pixels.append([1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1])
+# pixels.append([0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0])
+# pixels.append([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
+# pixels.append([0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0])
+# pixels.append([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0])
 
-for i in range(0, len(pixels)):
-    for j in range(0, len(pixels[i])):
-        if pixels[i][j] == 1:
-            box(boxSize)
-        myPen.pendown()
+
+#
+
+# print(pixels)
+# print(len(pixels[0]))
+# print(pixels[2])
+
+
+for i in range(0, len(grid)):
+    for j in range(0, len(grid[i])):
+        if grid[i][j] == 0:
+            emptyGrid(boxSize)
+            print("Printing grid row {0}".format(i))
+        else:
+            fillGrid(boxSize)
+        myPen.penup()
         myPen.forward(boxSize)
         myPen.pendown()
     myPen.setheading(270)
     myPen.penup()
     myPen.forward(boxSize)
     myPen.setheading(180)
-    myPen.forward(boxSize * len(pixels[i]))
+    myPen.forward(boxSize * len(grid[i]))
     myPen.setheading(0)
     myPen.pendown()
 
