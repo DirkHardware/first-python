6f2362248e1923c6126afe7985f082e8674a62e0
DirkHardware <email.the.anderson@gmail.com>
2020-08-27 21:53:18 -0400

    just something for today

---
+++ objects_song.py
@@ -0,0 +1,12 @@
+class Song:
+
+    def __init__(self, title, artist, duration):
+        self._title = title
+        self._artist = artist
+        self._duration = duration
+
+
+a_string = """this is\na string split\t\tand tabbed"""
+print(a_string)
+
+""" Foo """
