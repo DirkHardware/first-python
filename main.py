1c689e0c59614d2fcb3a0998cc0dfb7faf465ed2
DirkHardware <email.the.anderson@gmail.com>
2020-09-02 16:48:44 -0400

    my solution to the property challenge from ch. 259

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from player import Player
 anderson = Player("Anderson")
 
 print(anderson.name)
-print(anderson.lives)
-anderson.lives -= 1
-print(anderson.lives)
+# print(anderson.lives)
+# anderson.lives -= 1
+# print(anderson)
+#
+# anderson.lives -= 1
+# print(anderson)
+#
+# anderson.lives -= 1
+# print(anderson)
+#
+# anderson.lives -= 1
+# print(anderson)
 
-
+anderson.level = 3
+print(anderson.level)
+print(anderson.score)
--- player.py
+++ player.py
@@ -3,18 +3,46 @@ class Player(object):
     def __init__(self, name):
         self.name = name
         self._lives = 3
-        self.level = 1
+        self._level = 1
         self.score = 0
 
     def _get_lives(self):
         return self._lives
 
     def _set_lives(self, lives):
-        self._lives = lives
+        if lives >= 0:
+            self._lives = lives
+        else:
+            print("Lives cannot be negative")
+            self._lives = 0
+
+    def _get_level(self):
+        return self._level
+
+    def _set_level(self, new_level):
+        if new_level > 1:
+            if new_level > self._level:
+                score_bonus = new_level - self._level
+                self._level = new_level
+                self.score = self.score + (1000 * score_bonus)
+            elif new_level < self._level:
+                score_bonus = self._level - new_level
+                self._level = new_level
+                self.score = self.score - (1000 * score_bonus)
+        else:
+            print("Level cannot be less than 1 ")
+            self._level = 1
+            self.score = 0
 
     # This line allows the .lives values in main.py to work as they are
     # written without rewriting or calling the setter and getter functions
     lives = property(_get_lives, _set_lives)
+    level = property(_get_level, _set_level)
+
+    # This use of the string functions returns this string whenever an instance of the class
+    # is used as an argument in a print function
+    def __str__(self):
+        return "Name: {0.name}, Lives: {0.lives}, Level: {0.level}, Score: {0.score}".format(self)
 
 
 if __name__ == '__main__':
