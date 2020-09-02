e615452880b7e25423c3ba5061daa7deefec106c
DirkHardware <email.the.anderson@gmail.com>
2020-09-02 12:11:11 -0400

    first use of class properties

--- player.py
+++ player.py
@@ -2,9 +2,19 @@ class Player(object):
 
     def __init__(self, name):
         self.name = name
-        self.lives = 3
+        self._lives = 3
         self.level = 1
         self.score = 0
+
+    def _get_lives(self):
+        return self._lives
+
+    def _set_lives(self, lives):
+        self._lives = lives
+
+    # This line allows the .lives values in main.py to work as they are
+    # written without rewriting or calling the setter and getter functions
+    lives = property(_get_lives, _set_lives)
 
 
 if __name__ == '__main__':
