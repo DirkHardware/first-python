812847c1a7a171bfdb465dbf7049b70d86ac26ee
DirkHardware <email.the.anderson@gmail.com>
2020-09-18 14:04:35 -0400

    used composition to write out an html file

--- composition.py
+++ composition.py
@@ -1,4 +1,4 @@
-class Tag(object)
+class Tag(object):
 
     def __init__(self, name, contents):
         self.start_tag = '<{}>'.format(name)
@@ -6,10 +6,11 @@ class Tag(object)
         self.contents = contents
 
     def __str__(self):
-        return "{0.start_tag}{0.contents}{0.contents}".format(self)
+        return "{0.start_tag}{0.contents}{0.end_tag}".format(self)
 
-    def display(self):
-        print(self)
+    def display(self, file=None):
+        print(self, file=file)
+
 
 class DocType(Tag):
 
@@ -27,15 +28,44 @@ class Head(Tag):
 class Body(Tag):
 
     def __init__(self):
-        super().__init__(body, '') #body contents will be built up seperately
+        super().__init__('body', '') #body contents will be built up seperately
         self._body_contents = []
 
     def add_tag(self, name, contents):
         new_tag = Tag(name, contents)
         self._body_contents.append(new_tag)
 
-    def display(self):
-        for tag in self._body_contengs:
+    def display(self, file=None):
+        for tag in self._body_contents:
             self.contents += str(tag)
 
-        super().display
+        super().display(file=file)
+
+class HtmlDoc(object):
+
+    # That this class is made up primarily of other classes is an example of composition.
+    # It doesn't need to be made up entirely of other classes, it just needs to make use
+    # of some other classes as attributes and make use of what those other classes provide
+    def __init__(self):
+        self._doc_type = DocType()
+        self._head = Head()
+        self._body = Body()
+
+    def add_tag(self, name, contents):
+        self._body.add_tag(name, contents)
+
+    def display(self, file=None):
+        self._doc_type.display(file=file)
+        print('<html>', file=file)
+        self._head.display(file=file)
+        self._body.display(file=file)
+        print('</html>', file=file)
+
+
+if __name__ == '__main__':
+    my_page = HtmlDoc()
+    my_page.add_tag('h1', 'Main heading')
+    my_page.add_tag('h2', 'subheading')
+    my_page.add_tag('p', 'this is a paragraph that will appear on the page')
+    with open('test.html', 'w') as test_doc:
+        my_page.display(file=test_doc)
